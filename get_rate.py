148cb95b316a2bc8091a48d521a014503db2a2b6
pie <429224280@qq.com>
2019-07-17 22:32:48 +0800

    first commit

---
+++ get_rate.py
@@ -0,0 +1,22 @@
+import json
+from decimal import Decimal
+
+import requests
+import time
+
+num = 0
+sum = 0
+
+for i in range(1, 11):
+    url = "https://www.okex.me/v2/perpetual/pc/public/fundingRate?contract=BTC-USD-SWAP&current=" + str(
+        i) + "&t=" + str(time.time())
+    data = requests.get(url).text
+    data = json.loads(data)
+    list = data["data"]["fundingRates"]
+    for y in list:
+        num = num + 1
+        print(y["realFundingRate"])
+        sum = sum + Decimal(y["realFundingRate"])
+
+rate = sum / num
+print("okex永续合约每日平均费率:", rate * 3 * 100, "%")
