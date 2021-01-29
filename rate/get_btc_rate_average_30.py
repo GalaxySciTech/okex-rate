d23c6afe1343a76627418b111eda0b8a1594666c
pie <429224280@qq.com>
2021-01-29 21:42:01 +0800

    update

--- rate/get_btc_rate_average_30.py
+++ rate/get_btc_rate_average_30.py
@@ -7,16 +7,19 @@ import time
 num = 0
 sum = 0
 
-for i in range(1, 5):
-    url = "https://www.okex.me/v2/perpetual/pc/public/fundingRate?contract=BTC-USD-SWAP&current=" + str(
+for i in range(1, 14):
+    url = "https://www.okexcn.com/v2/perpetual/pc/public/fundingRate?contract=BTC-USD-SWAP&current=" + str(
         i) + "&t=" + str(time.time())
     data = requests.get(url).text
     data = json.loads(data)
     list = data["data"]["fundingRates"]
     for y in list:
-        num +=1
+        num += 1
         # print(y["realFundingRate"])
         sum += Decimal(y["realFundingRate"])
 
 rate = sum / num
-print("okex永续合约30日平均费率:", rate *3)
+ave_rate = rate * 3 * 100
+print("okex永续合约日息:", ave_rate, "%")
+print("okex永续合约月息", ave_rate * 30, "%")
+print("okex永续合约年息", ave_rate * 30 * 12, "%")
